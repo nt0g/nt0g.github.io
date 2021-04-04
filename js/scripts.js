cf20b5d4c8c1ced3891ea404d34927a5d80d8731
Nt0G <Nt0G@users.noreply.github.com>
2021-04-04 23:00:04 +0400

    button press animation changes
    Thank you, people of stackoverflow

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+window.onload = init;
+function init() {
+	if (window.Event) {
+	document.captureEvents(Event.MOUSEMOVE);
+	}
+	document.onmousemove = clickEvent;
+}
+var rt = document.querySelector(':root');
+function setDark() {
+  rt.style.setProperty('--bgcolor', 'rgb(10,10,10)');
+  rt.style.setProperty('--block-bgcolor', 'rgba(20,20,20,1)');
+  rt.style.setProperty('--block-contentcolor', 'rgba(31,31,31,1)');
+  rt.style.setProperty('--block-bordercolor', 'rgba(20,20,20,1)');
+  rt.style.setProperty('--accentcolor', 'rgba(120,140,200,1)');
+  rt.style.setProperty('--selcolor', '255');
+  rt.style.setProperty('--textcolor', 'rgba(245,245,245,1)');
+}
+function setLight() {
+  rt.style.setProperty('--bgcolor', 'rgb(250,240,240)');
+  rt.style.setProperty('--block-bgcolor', 'rgba(200,200,200,1)');
+  rt.style.setProperty('--block-contentcolor', 'rgba(255,255,255,1)');
+  rt.style.setProperty('--block-bordercolor', 'rgba(150,150,150,1)');
+  rt.style.setProperty('--accentcolor', 'rgba(120,140,200,1)');
+  rt.style.setProperty('--selcolor', '0');
+  rt.style.setProperty('--textcolor', 'rgba(5,5,5,1)');
+}
+function setDuration() {
+  var x = document.getElementById("duration").value;
+  rt.style.setProperty('--animation-duration', x + 'ms');
+}
+function setBlockWidth() {
+  var x = document.getElementById("bwidth").value;
+  rt.style.setProperty('--block-width', x + 'em');
+}
+function setBlockBorderRadius() {
+  var x = document.getElementById("bradius").value;
+  rt.style.setProperty('--borderradius', x + 'em');
+}
+function setPerspective() {
+  var x = document.getElementById("perspective").value;
+  rt.style.setProperty('--perspective', x + 'cm');
+}
+function clickEvent(click) {
+	var targ = click.target.getBoundingClientRect();
+	var hWid = targ.width / 2;
+	var hHei = targ.height / 2;
+	var xPos = click.clientX - targ.left - hWid;
+	var yPos = click.clientY - targ.top - hHei;
+	var xPer = xPos / hWid;
+	var yPer = -1 * yPos / hHei;
+	var xAng = Math.round(xPer * 15);
+	var yAng = Math.round(yPer * 15);
+	rt.style.setProperty('--yrot', xAng + 'deg');
+	rt.style.setProperty('--xrot', yAng + 'deg');
+    }
