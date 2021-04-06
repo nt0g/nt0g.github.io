ed0e2fec99e423370b4fe9dc18b647e98169f08e
Nt0G <Nt0G@users.noreply.github.com>
2021-04-06 18:24:09 +0400

    settings changes

--- js/settings.js
+++ js/settings.js
@@ -1,57 +1,14 @@
 var rt = document.querySelector(':root');
 
+function setSetting(variant,id,str1,str2) {
+	if (variant === 0) {
+		rt.style.setProperty(str1, str2);
+	} else if (variant === 1) {
+		let x = document.getElementById(id).value;
+		rt.style.setProperty(str1, x + str2);
+	}
+}
 
-function setDark() {
-  rt.style.setProperty('--bgcolor', 'rgb(10,10,10)');
-  rt.style.setProperty('--block-bgcolor', 'rgba(20,20,20,1)');
-  rt.style.setProperty('--block-contentcolor', 'rgba(31,31,31,1)');
-  rt.style.setProperty('--block-bordercolor', 'rgba(20,20,20,1)');
-  rt.style.setProperty('--accentcolor', 'rgba(120,140,200,1)');
-  rt.style.setProperty('--selcolor', '255');
-  rt.style.setProperty('--textcolor', 'rgba(245,245,245,1)');
-}
-function setLight() {
-  rt.style.setProperty('--bgcolor', 'rgb(250,240,240)');
-  rt.style.setProperty('--block-bgcolor', 'rgba(200,200,200,1)');
-  rt.style.setProperty('--block-contentcolor', 'rgba(255,255,255,1)');
-  rt.style.setProperty('--block-bordercolor', 'rgba(150,150,150,1)');
-  rt.style.setProperty('--accentcolor', 'rgba(120,140,200,1)');
-  rt.style.setProperty('--selcolor', '0');
-  rt.style.setProperty('--textcolor', 'rgba(5,5,5,1)');
-}
-function setDuration() {
-  var x = document.getElementById("duration").value;
-  rt.style.setProperty('--animation-duration', x + 'ms');
-}
-function setBlockWidth() {
-  var x = document.getElementById("bwidth").value;
-  rt.style.setProperty('--block-width', x + 'em');
-}
-function setBlockBorderRadius() {
-  var x = document.getElementById("bradius").value;
-  rt.style.setProperty('--borderradius', x + 'em');
-}
-function setBorderWidth() {
-  var x = document.getElementById("brdwidth").value;
-  rt.style.setProperty('--borderwidth', x + 'px');
-}
-function setPerspective() {
-  var x = document.getElementById("perspective").value;
-  rt.style.setProperty('--perspective', x + 'cm');
-}
-function set3dscale() {
-  var x = document.getElementById("3dscale").value;
-  rt.style.setProperty('--3d-scale', x);
-}
-function setSettingScale() {
-  rt.style.setProperty('--method', 'scale(0.975)');
-}
-function setSettingTranslate() {
-  rt.style.setProperty('--method', 'translateZ(-5px)');
-}
-function setSettingBoth() {
-  rt.style.setProperty('--method', 'translateZ(-5px) scale(var(--zsca))');
-}
 function playAnimation() {
   document.querySelector(".pulsar").className = "pulsar";
   window.requestAnimationFrame(function(time) {
@@ -60,3 +17,47 @@ function playAnimation() {
     });
   });
 }
+
+function loadFile(filePath) {
+	var result = null;
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET","themes/" + filePath + ".txt", false);
+	xmlhttp.send();
+	if (xmlhttp.status==200) {
+		result = xmlhttp.responseText;
+	}
+	return result;
+};
+
+function setTheme(choice) {
+	var plain = loadFile(choice);
+	var array = [];
+	array = plain.split(/\n|\r/g);
+	array.forEach(function(entry) {
+		var sndarray = []
+		sndarray = entry.split(/\|/);
+		str11 = sndarray[0];
+		str21 = sndarray[1];
+		setSetting(0,0,str11,str21);
+	});
+};
+
+var queryDark = window.matchMedia('(prefers-color-scheme: dark)');
+function handleDChange(e) {
+  if (e.matches) {
+    console.log('Dark!');
+	setTheme('dark');
+  }
+};
+queryDark.addListener(handleDChange);
+handleDChange(queryDark);
+
+var queryLight = window.matchMedia('(prefers-color-scheme: light)');
+function handleLChange(e) {
+  if (e.matches) {
+    console.log('Light!');
+	setTheme('light');
+  }
+};
+queryLight.addListener(handleLChange);
+handleLChange(queryLight);
