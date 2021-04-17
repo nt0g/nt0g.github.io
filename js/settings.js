6b406c6f5714a42af8984698acfe44e9464e2aaa
Nt0G <Nt0G@users.noreply.github.com>
2021-04-17 18:00:06 +0400

    fixed stupid stuff

--- js/settings.js
+++ js/settings.js
@@ -14,26 +14,3 @@ function saveSetting(variant,id,str1,str2,x) {
 	let arrSett = [variant, str1, str2, x];
 	localStorage.setItem(id, JSON.stringify(arrSett));
 }
-function loadSetting(variant,str1,str2,prev) {
-	switch (variant) {
-	case 0:
-		root.style.setProperty(str1, str2);
-		break;
-	case 1:
-		root.style.setProperty(str1, prev + str2);
-		break;
-	case 'theme':
-		getTheme(str2);
-		break;
-	}
-}
-function initSettings() {
-	for (i = 0; i < localStorage.length; i++) {
-		let varName = localStorage.key(i);
-//		console.log(varName);
-		let argSet = JSON.parse(localStorage.getItem(varName));
-//		console.log(argSet);
-		loadSetting(...argSet);
-	} 
-}
-document.addEventListener('DOMContentLoaded', initSettings);
