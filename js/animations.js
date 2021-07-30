4a522efa6b9d4cdbf358eb5b2e8010a59597124f
Nt0G <3647972+nt0g@users.noreply.github.com>
2021-07-30 15:41:15 +0400

    simplification, pt.1

--- js/animations.js
+++
@@ -1,8 +0,0 @@
-function pulse() {
-  document.querySelector('.pulsar').className = "pulsar";
-  window.requestAnimationFrame(function(time) {
-    window.requestAnimationFrame(function(time) {
-      document.querySelector('.pulsar').className = "pulsar pulse";
-    });
-  });
-}
