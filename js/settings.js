9de3020415989b53c6066ffadc55ace3dcd4040c
Nt0G <3647972+nt0g@users.noreply.github.com>
2021-08-03 14:02:10 +0400

    additions II

--- js/settings.js
+++ js/settings.js
@@ -1,23 +1,60 @@
-var root = document.querySelector(':root');
+let root = document.querySelector(':root');
 
-function setSetting(variant,id,str1,str2) {
-	switch (variant) {
+function saveSetting(type,id,name,value,prv) {
+	let arrSett = [type, id, name, value, prv];
+	localStorage.setItem(id, JSON.stringify(arrSett));
+}
+
+function setSetting(type,id,name,value,query) {
+	switch (type) {
 	case 0:
-		root.style.setProperty(str1, str2);
-		saveSetting(variant,id,str1,str2,"");
+		root.style.setProperty(name, value);
+		saveSetting(type,id,name,value,"");
 		break;
 	case 1:
 		let set = document.getElementById(id).value;
-		root.style.setProperty(str1, set + str2);
-		saveSetting(variant,id,str1,str2,set);
+		root.style.setProperty(name, set + value);
+		saveSetting(type,id,name,value,set);
 		break;
 	case 2:
-		document.documentElement.setAttribute(str1, str2);
-		saveSetting(variant,id,str1,str2,"");
+		document.documentElement.setAttribute(name, value);
+		saveSetting(type,id,name,value,"");
+		break;
+	case 'checkbox':
+		document.documentElement.setAttribute(name, value);
+		saveSetting(type,id,name,value,query);
 		break;
 	}
 }
-function saveSetting(variant,id,str1,str2,x) {
-	let arrSett = [variant, str1, str2, x];
-	localStorage.setItem(id, JSON.stringify(arrSett));
+function checkboxHandle(state, name) {
+	switch (state) {
+	case 'on':
+		switch (name) {
+		case 'data-mute':
+			console.log('Muted');
+			muted = true;
+			break;
+		}
+		break;
+	case 'off':
+		switch (name) {
+		case 'data-mute':
+			console.log('Unmuted');
+			muted = false;
+			break;
+		}
+		break;
+	}
+}
+function checkboxChange(id,name,query) {
+	if (document.getElementById(id).checked) {
+		checkboxHandle('on',name);
+		setSetting('checkbox',id,name,true,query);
+		if (muted === false) new Audio(audioClickOn).play();
+	}
+	else {
+		checkboxHandle('off',name);
+		setSetting('checkbox',id,name,false,query);
+		if (muted === false) new Audio(audioClickOff).play();
+	}
 }
--- js/settingsload.js
+++ js/settingsload.js
@@ -1,25 +1,91 @@
-function loadSetting(variant,str1,str2,prev) {
-	switch (variant) {
+let audioClick = 'sounds/click.ogg';
+let audioClickSw = 'sounds/click-switch.ogg';
+let audioClickOn = 'sounds/on.ogg';
+let audioClickOff = 'sounds/off.ogg';
+
+var muted = false;
+
+function updateCheckbox(id,name,value,query) {
+	let checkboxPending = document.getElementById(id);
+	let hasAttr = document.documentElement.hasAttribute(name);
+	let hasQuery = window.matchMedia(query).matches;
+	let getAttr = document.documentElement.getAttribute(name) === 'true';
+
+		console.log(checkboxPending);
+		console.log(getAttr);
+		console.log(hasQuery);
+	if ((getAttr === true) || (!hasAttr && hasQuery)) {
+		checkboxPending.checked = true;
+		console.log('true');
+	} else {
+		checkboxPending.checked = false;
+		console.log('false');
+	}
+}
+function resetCheckboxes() {
+    for (index = 0; index < document.querySelectorAll('input.switch').length; index++) {
+		document.querySelectorAll('input.switch')[index].checked = false;
+		console.log('Reset');
+	}
+}
+function loadSetting(type,id,name,value,prv) {
+	switch (type) {
 	case 0:
-		root.style.setProperty(str1, str2);
+		root.style.setProperty(name, value);
 		break;
 	case 1:
-		root.style.setProperty(str1, prev + str2);
+		root.style.setProperty(name, prv + value);
 		break;
 	case 2:
-		document.documentElement.setAttribute(str1, str2);
+		document.documentElement.setAttribute(name, value);
 		break;
+	case 'checkbox':
+		document.documentElement.setAttribute(name, value);
+		updateCheckbox(id,name,value,prv);
+		break;
+	}
+	switch (name) {
+	case 'data-mute':
+		console.log('Mute');
+		muted = true;
+		break;	
 	}
 }
 function initSettings() {
-	for (i = 0; i < localStorage.length; i++) {
-		let varName = localStorage.key(i);
-//		console.log(varName);
-		let argSet = JSON.parse(localStorage.getItem(varName));
-//		console.log(argSet);
-		loadSetting(...argSet);
-	} 
+	if (localStorage.length === 0) {
+			console.log('Empty');
+			resetCheckboxes();
+	} else {
+		for (i = 0; i < localStorage.length; i++) {
+			let varName = localStorage.key(i);
+//			console.log(i);
+//			console.log(varName);
+			let argSet = JSON.parse(localStorage.getItem(varName));
+//			console.log(argSet);
+			loadSetting(...argSet);
+		}
+	}
 }
+
+function initSounds() {
+	const cur =  document.querySelectorAll('.push-an');
+    let length = cur.length;
+    for (index = 0; index < length; index++) {
+		switch (cur[index].classList.contains('switch')) {
+		case true:
+			cur[index].addEventListener('pointerdown', function(event) {
+				if (muted === false && event.button === 0) new Audio(audioClickSw).play();
+			}, false);
+			break;
+		default:
+			cur[index].addEventListener('pointerdown', function(event) {
+			if (muted === false && event.button === 0) new Audio(audioClick).play();
+			}, false);
+			break;
+		}
+	}
+}
+
 function initClock(){
 	setInterval(function() {
 		var minutes = new Date().getMinutes();
@@ -29,13 +95,7 @@ function initClock(){
 	}, 1000);
 }
 
-function initCheckboxUpdate() {
-	if ((document.documentElement.hasAttribute("data-color-mode") && document.documentElement.getAttribute("data-color-mode") === 'dark') || (!document.documentElement.hasAttribute("data-color-mode") && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-		check.checked = true;
-	} else {
-		check.checked = false;
-	}
-}
+
 
 function initAnimatorTimer() {
 	window.setTimeout(function() {
@@ -43,7 +103,8 @@ function initAnimatorTimer() {
 	}, 500)
 }
 
+
 document.addEventListener('DOMContentLoaded', initSettings);
+document.addEventListener('DOMContentLoaded', initSounds);
 document.addEventListener('DOMContentLoaded', initClock);
-document.addEventListener('DOMContentLoaded', initCheckboxUpdate);
 document.addEventListener('DOMContentLoaded', initAnimatorTimer);
